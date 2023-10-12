e67a4125c152d06d14b8f6c4f411eb08b27656c7
Ahsan <ahsanaz461@gmail.com>
2023-10-12 07:58:41 +0700

    add create todo

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Todo App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const TodoList(title: 'Flutter Todo List App'),
+    );
+  }
+}
+
+class TodoList extends StatefulWidget {
+  const TodoList({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<TodoList> createState() => _TodoListState();
+}
+
+class _TodoListState extends State<TodoList> {
+  final List<Todo> _todos = <Todo>[];
+  final TextEditingController _textEditingController = TextEditingController();
+
+  void _addTodo(String todoName) {
+    setState(() {
+      _todos.add(Todo(todoName: todoName, completed: false));
+    });
+
+    _textEditingController.clear();
+  }
+
+  Future<void> _displayDialog() async {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Add new Todo'),
+          content: TextField(
+            controller: _textEditingController,
+            decoration: const InputDecoration(hintText: 'Insert your Todo'),
+            autofocus: true,
+          ),
+          actions: <Widget>[
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12))),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('Cancel'),
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12),
+                )),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  _addTodo(_textEditingController.text);
+                },
+                child: const Text('New'))
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        children: _todos.map((Todo todo) {
+          return TodoItem(todo: todo);
+        }).toList(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _displayDialog(),
+        tooltip: 'Add Todo',
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class Todo {
+  Todo({required this.todoName, required this.completed});
+  String todoName;
+  bool completed;
+}
+
+class TodoItem extends StatelessWidget {
+  TodoItem({required this.todo}) : super(key: ObjectKey(todo));
+  final Todo todo;
+
+  TextStyle? _getTextStyle(bool checked) {
+    // return nothing if not checked
+    if (!checked) return null;
+
+    return const TextStyle(
+        color: Colors.black87, decoration: TextDecoration.lineThrough);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: () {},
+      leading: Checkbox(
+        checkColor: Colors.green.shade400,
+        activeColor: Colors.red.shade500,
+        value: todo.completed,
+        onChanged: (value) {},
+      ),
+      title: Row(children: <Widget>[
+        Expanded(
+            child: Text(todo.todoName, style: _getTextStyle(todo.completed))),
+        IconButton(
+          icon: const Icon(Icons.delete, color: Colors.red),
+          alignment: Alignment.centerRight,
+          onPressed: () {},
+        )
+      ]),
+    );
+  }
+}
